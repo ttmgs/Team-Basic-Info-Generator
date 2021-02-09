54ff8aa86bfeab0277728312ddbd5832cde45849
Magdi Tiea <magditiea@gmail.com>
2021-02-09 18:35:53 -0500

    render path folder & file location updated

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "templates");
+const outputPath = path.join(OUTPUT_DIR, "main.html");
 
 const render = require("./lib/htmlRenderer");
 const employeeTypes = ["Manager", "Engineer", "Intern"];
