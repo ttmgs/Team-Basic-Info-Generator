b44836caacdfb794c2e06cc8528531f5b0d70b26
Magdi Tiea <magditiea@gmail.com>
2021-02-09 18:21:27 -0500

    inquirer function added in app.js file

--- app.js
+++ app.js
@@ -9,35 +9,213 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const employeeTypes = ["Manager", "Engineer", "Intern"];
+const employees= [];
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-const managerinfo = new Manager [
-    {
-        id: userInput,
-        emai: "johndoe@gmail.com",
-        office: "1"
-    }
-],
+function addEmployee(){
+  inquirer    
+      .prompt([
+          {
+              type: 'list',
+              message: "What type of employee would you like to add?",
+              choices: employeeTypes,
+              name: 'employee',
+          },
+      ]).then(data =>{
+          switch(data.employee){
+              case("Manager"):
+                  addManager()
+                  break;            
+              case("Engineer"):
+                  addEngineer()
+                  break;            
+              case("Intern"):
+                  addIntern()
+                  break;
+          }
+      });
+}
 
-const engineerinfo = new Engineer [
-    {
-        id: "313",
-        email: "smith@gmail.com",
-        github: "smith223"
-    }
-]
+function addManager(){
+  console.log("Manager");
+  inquirer    
+      .prompt([
+          {
+              type: "input",
+              message: "Whats the name of your manager?",
+              name: "name",
+          },
+          {
+              type: "input",
+              message: "Whats the managers Id number?",
+              name: "id",
+          },
+          {
+              type: "input",
+              message: "Whats the managers email? (ex. email@email.com)",
+              name: "email",
+          },
+          {
+              type: "input",
+              message: "Whats the managers office number?",
+              name: "office",
+          },
+      ]).then(data =>{
+          if(!checkGeneralInfo(data) || !checkOfficeNumber(data.office))
+          {
+              addManager();
+          }
+          else{
+              employees.push(new Manager(data.name, data.id, data.email, data.office));
+              finish();
+          }
+      });
+}
 
-const interninfo = new Intern [
-    {
-        id: "314",
-        email: "alex@gmail.com",
-        school: "University of Texas"
-    }
-]
+function addEngineer(){
+  console.log("Engineer");
+  inquirer    
+      .prompt([
+          {
+              type: "input",
+              message: "Whats the name of your engineer?",
+              name: "name",
+          },
+          {
+              type: "input",
+              message: "Whats the engineers Id number?",
+              name: "id",
+          },
+          {
+              type: "input",
+              message: "Whats the engineers email? (ex. email@email.com)",
+              name: "email",
+          },
+          {
+              type: "input",
+              message: "Whats the engineers github username?",
+              name: "github",
+          },
+      ]).then(data =>{
+          if(!checkGeneralInfo(data) || !checkGithubUsername(data.github))
+          {
+              addEngineer();
+          }
+          else{
+              employees.push(new Engineer(data.name, data.id, data.email, data.github));
+              finish();
+          }
 
+      });
+}
+
+function addIntern(){
+  console.log("Intern");
+  inquirer    
+      .prompt([
+          {
+              type: "input",
+              message: "Whats the name of your intern?",
+              name: "name",
+          },
+          {
+              type: "input",
+              message: "Whats the interns Id number?",
+              name: "id",
+          },
+          {
+              type: "input",
+              message: "Whats the interns email? (ex. email@email.com)",
+              name: "email",
+          },
+          {
+              type: "input",
+              message: "What school is the intern attending?",
+              name: "school",
+          },
+      ]).then(data =>{
+          if(!checkGeneralInfo(data) || !checkSchool(data.school))
+          {
+              addIntern();
+          }
+          else{
+              employees.push(new Intern(data.name, data.id, data.email, data.school));
+              finish();
+          }
+      });
+}
+
+function finish()
+{
+  inquirer
+      .prompt([
+          {
+              type: "confirm",
+              message: "Would you like to add another employee?",
+              name: "confirmation",
+          }
+      ]).then(data => {
+          if(data.confirmation){
+              addEmployee();
+          }
+          else{
+              fs.writeFile(outputPath, render(employees), 'utf8',  (err) => err ? console.error(err) : console.log('Success! Team page created!'));
+          }
+      })
+}
+
+function checkGeneralInfo(data){
+  if(!data.name)
+  {
+      console.error("\x1b[31m", "Please enter a valid name.");
+      return false;
+  }
+  if(!data.id || isNaN(data.id)){
+      console.error("\x1b[31m", "Please enter a valid id.");
+      return false;
+  }
+  if(!validateEmail(data.email))
+  {
+      console.error("\x1b[31m", "Please enter a valid email.");
+      return false;
+  }
+  return true;
+}
+
+function checkOfficeNumber(num){
+  if(isNaN(num)){
+      console.error("\x1b[31m", "Please enter a valid office number.");
+      return false;
+  }
+  return true;
+}
+
+function checkGithubUsername(user){
+  if(!user){
+      console.error("\x1b[31m", "Please enter a valid Github username.");
+      return false;
+  }
+  return true;
+}
+
+function checkSchool(school){
+  if(!school){
+      console.error("\x1b[31m", "Please enter a valid school.");
+      return false;
+  }
+  return true;
+}
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+addEmployee();
 
 
 
